21bf0b92ed5b3ab23723416bb0a14bcf702c7b8b
Aleksey Cherepanov <ftp27host@gmail.com>
2014-01-31 17:31:58 +0400

    Add base project config

---
+++ obj/local/armeabi/objs-debug/android_native_app_glue/android_native_app_glue.o.d
@@ -0,0 +1,194 @@
+obj/local/armeabi/objs-debug/android_native_app_glue/android_native_app_glue.o: \
+ /home/ftp27/adt-bundle-linux/ndk/sources/android/native_app_glue/android_native_app_glue.c \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/jni.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/errno.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/linux/errno.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm/errno.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm-generic/errno.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm-generic/errno-base.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/string.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/malloc.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/unistd.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/stdint.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/_wchar_limits.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/select.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/time.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/linux/time.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/signal.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/limits.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/limits.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/linux/limits.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/machine/internal_types.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/machine/limits.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/syslimits.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm/page.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm/signal.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm-generic/signal.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm/sigcontext.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm/siginfo.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/sysconf.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/linux/capability.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/pathconf.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/resource.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/linux/resource.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm/resource.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm-generic/resource.h \
+ /home/ftp27/adt-bundle-linux/ndk/sources/android/native_app_glue/android_native_app_glue.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/poll.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/linux/poll.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm/poll.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/pthread.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/time.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sched.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/android/configuration.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/android/asset_manager.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/android/looper.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/android/native_activity.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/android/input.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/android/keycodes.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/android/native_window.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/android/rect.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/android/log.h
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/jni.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/errno.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/linux/errno.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm/errno.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm-generic/errno.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/string.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/unistd.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/select.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/time.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/linux/time.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/signal.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/limits.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/limits.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/linux/limits.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/machine/internal_types.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/machine/limits.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/syslimits.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm/page.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm/signal.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm-generic/signal.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm/sigcontext.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm/siginfo.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/sysconf.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/linux/capability.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/pathconf.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/resource.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/linux/resource.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm/resource.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm-generic/resource.h:
+
+/home/ftp27/adt-bundle-linux/ndk/sources/android/native_app_glue/android_native_app_glue.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/poll.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/linux/poll.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/asm/poll.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/pthread.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/time.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sched.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/android/configuration.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/android/asset_manager.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/android/looper.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/android/native_activity.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/android/input.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/android/keycodes.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/android/native_window.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/android/rect.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/android/log.h:
---
+++ obj/local/armeabi/objs/FirstGame/FirstGame.o.d
@@ -0,0 +1,13 @@
+obj/local/armeabi/objs/FirstGame/FirstGame.o: jni/FirstGame.cpp \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/jni.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/android/api-level.h
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/jni.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/ftp27/adt-bundle-linux/ndk/platforms/android-9/arch-arm/usr/include/android/api-level.h:
